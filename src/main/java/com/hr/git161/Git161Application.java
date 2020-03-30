d7f208dda4bd5aaea50311b90d262a6f40488eda
yifuren <czy29979@163.com>
2020-03-30 14:46:30 +0800

    第一次提交到本地仓库

---
+++ src/main/java/com/hr/git161/Git161Application.java
@@ -0,0 +1,13 @@
+package com.hr.git161;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git161Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git161Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hr/git161/Git161ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr.git161;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git161ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
